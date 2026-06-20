1e0d40d198d716874b30bb0708fa2c2acbfbe248
Harun <your@email.com>
2026-06-20 08:52:29 +0200

    data loader tweaked

--- src/main/java/management_workflow_api/DemoDataLoader.java
+++ src/main/java/management_workflow_api/DemoDataLoader.java
@@ -4,6 +4,7 @@ import management_workflow_api.Entity.Employee;
 import management_workflow_api.Entity.Manager;
 import management_workflow_api.Entity.Task;
 import management_workflow_api.Entity.TaskAssignment;
+import management_workflow_api.Entity.WebUser;
 import management_workflow_api.Entity.WorkGroup;
 import management_workflow_api.Repository.EmployeeRepository;
 import management_workflow_api.Repository.ManagerRepository;
@@ -12,6 +13,7 @@ import management_workflow_api.Repository.TaskRepository;
 import management_workflow_api.Repository.WorkGroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -26,6 +28,7 @@ public class DemoDataLoader implements CommandLineRunner {
     private final EmployeeRepository employeeRepository;
     private final TaskRepository taskRepository;
     private final TaskAssignmentRepository taskAssignmentRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) {
@@ -34,45 +37,137 @@ public class DemoDataLoader implements CommandLineRunner {
             return;
         }
 
+        // -------------------------
+        // Manager + corresponding WebUser
+        // -------------------------
+
+        WebUser managerUser = new WebUser();
+        managerUser.setUsername("manager");
+        managerUser.setPassword(passwordEncoder.encode("password"));
+        managerUser.setRole("MANAGER");
+
         Manager manager = new Manager();
         manager.setName("Demo Manager");
+        manager.setWebUser(managerUser);
+
+        managerUser.setManager(manager);
+
         managerRepository.save(manager);
+
+        // -------------------------
+        // WorkGroup
+        // -------------------------
 
         WorkGroup workGroup = new WorkGroup();
         workGroup.setName("Demo Workgroup");
         workGroup.setManager(manager);
+
         workGroupRepository.save(workGroup);
+
+        // -------------------------
+        // Employee 1 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee1User = new WebUser();
+        employee1User.setUsername("ahmed");
+        employee1User.setPassword(passwordEncoder.encode("password"));
+        employee1User.setRole("EMPLOYEE");
 
         Employee employee1 = new Employee();
         employee1.setName("Ahmed");
         employee1.setLanguage(Employee.Language.Arabic);
         employee1.setWorkGroup(workGroup);
+        employee1.setWebUser(employee1User);
+
+        employee1User.setEmployee(employee1);
+
+        // -------------------------
+        // Employee 2 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee2User = new WebUser();
+        employee2User.setUsername("murat");
+        employee2User.setPassword(passwordEncoder.encode("password"));
+        employee2User.setRole("EMPLOYEE");
 
         Employee employee2 = new Employee();
         employee2.setName("Murat");
         employee2.setLanguage(Employee.Language.Turkish);
         employee2.setWorkGroup(workGroup);
+        employee2.setWebUser(employee2User);
+
+        employee2User.setEmployee(employee2);
+
+        // -------------------------
+        // Employee 3 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee3User = new WebUser();
+        employee3User.setUsername("anna");
+        employee3User.setPassword(passwordEncoder.encode("password"));
+        employee3User.setRole("EMPLOYEE");
 
         Employee employee3 = new Employee();
         employee3.setName("Anna");
         employee3.setLanguage(Employee.Language.Polish);
         employee3.setWorkGroup(workGroup);
+        employee3.setWebUser(employee3User);
+
+        employee3User.setEmployee(employee3);
+
+        // -------------------------
+        // Employee 4 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee4User = new WebUser();
+        employee4User.setUsername("fatima");
+        employee4User.setPassword(passwordEncoder.encode("password"));
+        employee4User.setRole("EMPLOYEE");
 
         Employee employee4 = new Employee();
         employee4.setName("Fatima");
         employee4.setLanguage(Employee.Language.Arabic);
         employee4.setWorkGroup(workGroup);
+        employee4.setWebUser(employee4User);
+
+        employee4User.setEmployee(employee4);
+
+        // -------------------------
+        // Employee 5 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee5User = new WebUser();
+        employee5User.setUsername("ayse");
+        employee5User.setPassword(passwordEncoder.encode("password"));
+        employee5User.setRole("EMPLOYEE");
 
         Employee employee5 = new Employee();
         employee5.setName("Ayse");
         employee5.setLanguage(Employee.Language.Turkish);
         employee5.setWorkGroup(workGroup);
+        employee5.setWebUser(employee5User);
+
+        employee5User.setEmployee(employee5);
+
+        // -------------------------
+        // Employee 6 + corresponding WebUser
+        // -------------------------
+
+        WebUser employee6User = new WebUser();
+        employee6User.setUsername("kasia");
+        employee6User.setPassword(passwordEncoder.encode("password"));
+        employee6User.setRole("EMPLOYEE");
 
         Employee employee6 = new Employee();
         employee6.setName("Kasia");
         employee6.setLanguage(Employee.Language.Polish);
         employee6.setWorkGroup(workGroup);
+        employee6.setWebUser(employee6User);
 
+        employee6User.setEmployee(employee6);
+
+        // Because Employee -> WebUser has CascadeType.ALL,
+        // saving the employees also saves their WebUser objects.
         employeeRepository.saveAll(List.of(
                 employee1,
                 employee2,
@@ -81,6 +176,10 @@ public class DemoDataLoader implements CommandLineRunner {
                 employee5,
                 employee6
         ));
+
+        // -------------------------
+        // Tasks
+        // -------------------------
 
         Task task1 = new Task();
         task1.setTitle("Prepare onboarding document");
@@ -95,6 +194,10 @@ public class DemoDataLoader implements CommandLineRunner {
         task2.setManager(manager);
 
         taskRepository.saveAll(List.of(task1, task2));
+
+        // -------------------------
+        // TaskAssignments
+        // -------------------------
 
         TaskAssignment assignment1 = new TaskAssignment();
         assignment1.setName("Arabic onboarding translation");
